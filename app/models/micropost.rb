9f606d8fdc0660833208b1bf52ec54fb9c2b5dca
akira.enami <tarako2533@outlook.com>
2021-01-18 18:04:04 +0000

    dependent destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
 
-  has_many :favorites
-  has_many :favorite_microposts, through: :favorites, source: :microposts
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_microposts, through: :favorites, source: :micropost
 
   validates :content, presence: true, length: { maximum: 255}
 end
